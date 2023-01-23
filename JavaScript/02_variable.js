64f32babb400879e8201d8900482d6e9fd8cbb14
Snehal <snehalgadge205@gmail.com>
2023-01-23 20:13:57 +0530

    Basic js added

---
+++ JavaScript/02_variable.js
@@ -0,0 +1,16 @@
+var userName = 'Snehal';      //string with double quotation
+console.log(userName);
+
+var userAge = 23;              //integer
+console.log(userAge);
+
+let isLoggedIn = true;         //boolean
+console.log(isLoggedIn);
+
+const id = '123abc';           //string with single quotation
+console.log(id);
+
+let course;                   //undefined
+console.log(course);
+course = FSJS;
+console.log(course);
---
+++ JavaScript/03_datatype.js
@@ -0,0 +1,16 @@
+// typeof gives us the type of datatype
+
+var userName = 'Snehal';      
+console.log(typeof userName);
+
+var userAge = 23;              
+console.log(`the type of ${userAge} is ${typeof userAge}`);
+
+let isLoggedIn = true;         
+console.log(`the type of ${isLoggedIn} is `,typeof isLoggedIn);
+
+const id = '123abc';           
+console.log(`the type of ${id} is ${typeof id}`);
+
+let course;                 
+console.log(`the type of ${course} is ${typeof course}`);
