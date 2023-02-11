3039f5dfe8906ef7e1ddf58c2ee6c15886aab971
Snehal <snehalgadge205@gmail.com>
2023-02-11 21:05:58 +0530

    new topic learnt

--- JavaScript/13_dom1.js
+++ JavaScript/13_dom1.js
@@ -3,17 +3,24 @@
 
 // accessing first child
 // ->syntax:-  element.firstChild
+//             element.firstElementChild
 console.log(document.body.firstChild);
 console.log(document.body.childNodes[0]);      //[0] representing first child of all child nodes
+console.log(document.body.firstElementChild);  //gives exact element
+
 
 // accessing last child
 // ->syntax:-  element.lastChild
+//             element.lastElementChild
 console.log(document.body.lastChild);
-console.log(document.body.childNodes[document.body.childNodes.length-1]); 
+console.log(document.body.childNodes[document.body.childNodes.length-1]);
+console.log(document.body.lastElementChild);
+
 
 // accessing child Nodes
 // ->syntax:-  element.childNodes
 console.log(document.body.childNodes);
+
 
 // Check has child nodes ->it returns true if element has child and vice versa
 // ->synatx:- element.hasChildNodes()
---
+++ JavaScript/13_dom2.js
@@ -0,0 +1,4 @@
+// Accessing Parent  
+let a = document.body.firstChild;
+console.log(a.parentElement);      //parent elements can ne null
+console.log(a.parentNode);         //parent node cannot be null
