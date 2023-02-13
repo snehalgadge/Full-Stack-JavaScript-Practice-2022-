c8c2dc5360904f7f2531f978fdc69d1affe8250b
Snehal <snehalgadge205@gmail.com>
2023-02-13 22:39:48 +0530

    events

---
+++ JavaScript/14_event.js
@@ -0,0 +1,21 @@
+// An event is a signal that something has happened
+// DOM events
+// ->i)Mouse events:- click, context menu(right click), mouseOver/ mouseOut,mouseDrop/mouseUp,mousemove
+// ->ii)KeyBoard events:- keydown , keyup
+// ->iii)form element events:- submit ,focus ,etc
+// ->iv)Document events:- DOMContent Loaded
+
+// Example
+// i)
+// let a = document.querySelector("button");
+// a.onclick = function(){
+//     alert("Snehal clicked this");
+// };
+
+// ii)
+// let b = document.querySelector("button");
+// b.onclick = () =>{
+//     let a = document.querySelector("h1");
+//     a.innerText = "Mouse Clicked Event Happened"
+// };
+
