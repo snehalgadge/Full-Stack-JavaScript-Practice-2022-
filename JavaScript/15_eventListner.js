ccc25ec6453908d5ba5661707ca6c3d20a02214c
Snehal <snehalgadge205@gmail.com>
2023-02-16 17:19:48 +0530

    eventListener

--- JavaScript/15_eventListner.js
+++ JavaScript/15_eventListner.js
@@ -1,1 +1,22 @@
-// <!-- addEventListner is use to assign multiple handlers to an event -->git
+// <!-- addEventListner is use to assign multiple handlers to an event -->
+// Syntax 
+// -> element.addEventListner(event,handler);
+// -> element.removeEventListner(event,handler); handler must be of same function object
+
+let btn = document.querySelector(".btn");
+
+let x = function(e){
+    alert("Hello 1");
+}
+btn.addEventListener('click',x);
+
+let y = function(e){
+    alert("Hello 2");
+}
+btn.addEventListener('click',y);
+
+let a = prompt("Write a number");
+
+if(a==2){
+    btn.removeEventListener('click',x);
+}
