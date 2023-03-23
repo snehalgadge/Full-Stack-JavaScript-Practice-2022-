a74047d4156f53cd277c5c31fd12b53c8d41e4f2
Snehal <snehalgadge205@gmail.com>
2023-03-23 18:23:40 +0530

    react started

---
+++ React/react-01-basic/src/App.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+// New version
+
+// const App = () => {
+//   return(
+//     <div>
+//       Helloo World !
+//     </div>
+//   )
+// }
+
+
+// Old verison :this version needs the import React from react statement
+
+// const App = () =>{
+//   return React.createElement("h1", {}, "Hello World");
+// }
+
+
+// React Fragment
+const App = () => {
+  return(
+    <>
+     <h1 className="hello">Hello Everyone {3+3}</h1>
+     <p>This example is illustrating React Fragment</p>
+
+     <MyName/>   {/* nested component */}
+    </>
+  );
+};
+
+
+// Creating a new component which print my name
+
+const MyName = () =>{
+  return(
+  <>
+    <h1>My name is Snehal</h1>
+  </>
+  );
+};
+
+export default App;
+
+
+
